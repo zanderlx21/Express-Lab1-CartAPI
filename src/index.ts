ceaffd1744c460b45d373b144585b1c3ab18ce03
Alex Zaks <zaksa86@gmail.com>
2022-01-04 14:19:18 -0500

    adding PUT

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,7 @@ import routes from "./routes/cart-items-routes"
 const app = express();
 
 app.use(express.json());
-app.use("/", routes);
 app.use("/cart-items", routes);
-app.use("/cart-items/id:", routes);
 
 const port = 3008;
 
--- src/routes/cart-items-routes.ts
+++ src/routes/cart-items-routes.ts
@@ -1,33 +1,78 @@
-import express from 'express';
+import express from "express";
 import Cart from "../models/cart";
 
 const routes = express.Router();
 
 const cartItems: Cart[] = [
-    { id: 1, product: "Slippers", price: 17.99, quantity: 12},
-    { id: 2, product: "Pajamas", price: 34.99, quantity: 10},
-    { id: 3, product: "Scarf", price: 22.99, quantity: 30},
-    { id: 4, product: "Hat", price: 24.99, quantity: 15},
-    { id: 5, product: "Socks", price: 12.99, quantity: 25}
-] 
+  { id: 1, product: "Slippers", price: 17.99, quantity: 12 },
+  { id: 2, product: "Pajamas", price: 34.99, quantity: 10 },
+  { id: 3, product: "Scarf", price: 22.99, quantity: 30 },
+  { id: 4, product: "Hat", price: 24.99, quantity: 15 },
+  { id: 5, product: "Socks", price: 12.99, quantity: 25 },
+];
+
+let nextID: number = 6;
 
 export default routes;
 
 routes.get("/", function (request, response) {
-  
+  let MaxPrice: number = parseInt(request.query.maxPrice as string);
+  let Prefix: string = request.query.prefix as string;
+  let PageSize = Number.parseInt(request.query.pageSize as string);
+  let cartArray: Cart[] = [];
+
+  if (MaxPrice) {
+    let queryPrice: number = Number.parseFloat(
+      request.query.maxPrice as string
+    );
+    let filteredPrice: Cart[] = cartItems.filter(
+      (item) => item.price <= queryPrice
+    );
+    response.json(filteredPrice);
+  }
+  if (Prefix) {
+    let filteredPrefix: Cart[] = cartItems.filter((item) =>
+      item.product.toLowerCase().startsWith(Prefix.toLowerCase())
+    );
+    response.json(filteredPrefix);
+  }
+  if (PageSize) {
+    let filteredPage = cartItems.filter((item, index) => index <= PageSize - 1);
+    response.json(filteredPage);
+  } else {
+    response.json(cartItems);
+  }
 });
 
-routes.get("/cart-items", function (request, response) {
-    let queryPrice = Number.parseInt(request.query.maxPrice as string);
-    let queryPrefix = Number.parseInt(request.query.prefix as string);
-    let queryPage = Number.parseInt(request.query.pageSize as string);
+// routes.get("cart-items/:id", function (req, res) {
+// 	const item = cartItems.find((item) => item.id === parseInt(req.params.id));
+// 	item ? res.json(item) : res.status(404).send("ID Not Found");
+// });
+
+routes.get("/:id", function (request, response) {
+  for (let i = 0; i < cartItems.length; i++) {
+    let inputID: number = Number.parseInt(request.params.id);
+    if (cartItems[i].id === inputID) {
+      response.status(200).json(cartItems[i]);
+      break;
+    }
+  }
+  response.status(404);
+  response.send({ error: "ID not found" });
 });
 
-routes.get("/cart-items/:id", function (request, response) {
-//     if (){
-//     response.status(200);
-// } else {
-    response.status(404);
-    response.send({ error: "ID not found" });
-// }
+routes.post("/", function (request, response) {
+  let newItem: Cart = request.body;
+  newItem.id = nextID;
+  nextID += 1;
+  cartItems.push(newItem);
+  response.status(201).json(cartItems);
+});
+
+routes.put("/:id", function (request, response) {
+    let itemUpdates: Cart = request.body;
+    for (let i  =0 ; i < cartItems.length; i++) {
+
+    }
+    
 });
